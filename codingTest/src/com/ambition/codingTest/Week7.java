65f09cc817b62b212912b7a389a82301cf329e4f
송다인 <inhere31@gmail.com>
2023-03-08 10:57:22 +0900

    week7

--- codingTest/src/com/ambition/codingTest/Week7.java
+++ codingTest/src/com/ambition/codingTest/Week7.java
@@ -1,18 +1,93 @@
 package com.ambition.codingTest;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class Week7 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-		System.out.println("프로그래머스 문제");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		long number = 12345;
+		// 백준 1259
 		
-		long reverse = number;
+		
+		while(true) {
+			
+			System.out.print("숫자 입력 : ");
+			String str2 = br.readLine();
+			boolean check = true;
+			
+			// 0을 입력하면 종료
+			if(str2.equals("0")) break;
+			
+			// 숫자를 반으로 나누어 맨앞자리와 맨뒤를 돌며 비교
+			for(int i = 0; i < str2.length()/2; i++) {
+				if(str2.charAt(i) != str2.charAt(str2.length() -1 -i)) check = false;
+			}
+			
+			if(check) System.out.println("yes");
+			else System.out.println("no");
+		}
+		
+		
+		
+		// 백준 1152 ========================
+		
+		System.out.print("문자열 입력: ");
+		
+		// StringTokenizer로 공백을 통해 분리하여 토큰에 저장 후 토큰 세기
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		System.out.println(st.countTokens());
+		
+		
+		// 다른 풀이
+		
+		int count = 0;
+		int pre_str = 32; // 공백을 의미
+		int str;
+		
+		while(true) {
+			
+			// 문자를 아스키코드로(정수형으로) 입력받기
+			str = System.in.read();
+			
+			// 입력받은 문자가 공백일 때
+			if(str == 32) {
+				// 이전 문자가 공백이 아니면
+				if(pre_str != 32) count++;
+			}
+			
+			// 입력받은 문자가 개행일 때
+			else if(str == 10) {
+				// 이전의 문자가 공백이 아니면
+				if(pre_str != 32) count++;
+				break;
+			}
+			
+			pre_str = str;
+			
+		}
+		
+		System.out.println(count);
+		
+		
+		
+		// 프로그래머스 =======================
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자 입력: ");
+		long number = sc.nextLong();
+		
+		int reverse = (int) number;
 		int k = 0;
 
 		
-		// 먼저 입력받은 number의 길이를 알아봄(변수 k에 넣음)
+		// 먼저 입력받은 number의 길이를 변수 k에 넣는 작업
 		while(number > 0) {
 			k++;
 			number = number/10;
@@ -20,17 +95,20 @@ public class Week7 {
 		
 		int arr[] = new int[k];
 		
+		
+		// 끝자리 수가 나머지가 되어 가져올 수 있음
 		for(int i = 0; i < k; i++) {
-			arr[i] = (int)reverse%10;
+			arr[i] = reverse%10;
 			reverse /=10;
 		}
 		
+		// 출력
 		for(int num : arr) {
 			System.out.print(num + " ");
 		}
 		
 		
-		
+		sc.close();
 		
 
 	}
